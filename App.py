3da5e1f050d47b09322736a774887917bf39e47b
161250034 <coreferry@gmail.com>
2018-09-23 09:22:50 +0800

    完成(可能存在年份的bug)

--- App.py
+++ App.py
@@ -1,13 +1,11 @@
-import string
-
 from flask import Flask
 from flask_apscheduler import APScheduler
+from datetime import datetime
 import tushare as ts
 import json
-
-from pandas import DataFrame
-
+import pandas as pd
 import DBHandler as db
+import numpy as np
 from DBHandler import Strategy
 
 '''
@@ -36,6 +34,34 @@ app.config.from_object(Config())
 scheduler=APScheduler()
 scheduler.init_app(app)
 scheduler.start()
+
+# 没有使用numpy的后果
+def list_add(a,b):
+	c = []
+	for i in range(len(a)):
+		c.append(a[i]+b[i])
+	return c
+
+
+#返回包括此月份的前12个月
+def getOneYearMonthList(a):
+	yearNow = int(a.split('-')[0])
+	monthNow = int(a.split('-')[1])
+	retList = []
+	if monthNow == 12:
+		for i in range(12):
+			retList.append(str(yearNow) + '-' + str(i+1))
+	else:
+		monthBegin = monthNow+1
+		monthBeforeNum = 12 - monthNow
+		for i in range(monthBeforeNum):
+			retList.append(str(yearNow-1) + '-' + str(monthBegin+i))
+		for i in range(12 -monthBeforeNum):
+			retList.append(str(yearNow) + '-' + str(i+1))
+	return retList
+
+
+
 
 
 @app.route('/stock/<string:code>')
@@ -68,13 +94,191 @@ def getAStock(code):
 
 	return json.dumps(result,ensure_ascii=False)
 
-# 根据recordId获得需要回测的策略
-# recordId是long
+
+@app.route('/strategy/recommend/profit')
+def getProfitInfo():
+	result={}
+	stocks=[]
+
+	tempProfitDic = getProfitDic()
+	result['risk']=tempProfitDic.get('var')
+	result['todayBenefit']=tempProfitDic.get('value')
+	templist =getTopX()
+	profitList = [0,0,0,0,0,0,0,0,0,0,0,0]
+	for i in range(10):
+		stockId =templist[i][0]
+		temp = {}
+		temp['buyRate'] = tempProfitDic.get(stockId)
+		tempProfit =getMonthReturnPerYear(stockId)
+		for i in range(12):
+			tempProfit[i]=tempProfit[i]*temp['buyRate']
+		profitList =list_add(profitList,tempProfit)
+		temp['stockId']=stockId
+		stock=getAStockBasicData(stockId)
+		temp['stockName']=stock.name
+		temp['currentPrice']=stock.trade
+		temp['trend']=stock.changepercent
+		temp['turnoverRate']=stock.turnoverratio
+		temp['marketProfitability']=stock.per
+		temp['todayVolume']=stock.volume
+
+		stocks.append(temp)
+	result['stocks']=stocks
+
+	loopbackData = []
+	loopbackPeryear =getSZData('2018-06')
+	monthList =getOneYearMonthList('2018-06')
+	for i in range(12):
+		temp = {}
+		temp['data']= monthList[i]
+		temp['上证指数']= loopbackPeryear.get(temp['data'])
+		temp['自选股'] = profitList[i]
+		loopbackData.append(temp)
+
+	result['loopback'] = loopbackData
+
+
+	return json.dumps(result,ensure_ascii=False)
+
+@app.route('/strategy/recommend/risk')
+def getRiskInfo():
+	result={}
+	stocks=[]
+
+	tempRiskDic = getRiskDic()
+	result['risk']=tempRiskDic.get('var')
+	result['todayBenefit']=tempRiskDic.get('value')
+	templist =getTopX()
+	profitList = [0,0,0,0,0,0,0,0,0,0,0,0]
+	for i in range(10):
+		stockId =templist[i][0]
+		temp = {}
+		temp['buyRate'] = tempRiskDic.get(stockId)
+		tempProfit =getMonthReturnPerYear(stockId)
+		for i in range(12):
+			tempProfit[i]=tempProfit[i]*temp['buyRate']
+		profitList =list_add(profitList,tempProfit)
+		temp['stockId']=stockId
+		stock=getAStockBasicData(stockId)
+		temp['stockName']=stock.name
+		temp['currentPrice']=stock.trade
+		temp['trend']=stock.changepercent
+		temp['turnoverRate']=stock.turnoverratio
+		temp['marketProfitability']=stock.per
+		temp['todayVolume']=stock.volume
+
+		stocks.append(temp)
+	result['stocks']=stocks
+
+	loopbackData = []
+	loopbackPeryear =getSZData('2018-06')
+	monthList =getOneYearMonthList('2018-06')
+	for i in range(12):
+		temp = {}
+		temp['data']= monthList[i]
+		temp['上证指数']= loopbackPeryear.get(temp['data'])
+		temp['自选股'] = profitList[i]
+		loopbackData.append(temp)
+
+	result['loopback'] = loopbackData
+
+
+	return json.dumps(result,ensure_ascii=False)
+
 def getStrategy(recordId):
 	rid=int(recordId)   # python3中整数就是long
 	session=db.setup_db()
 	strategy=session.query(Strategy).filter(Strategy.id==rid).one()
 	return strategy
+
+@app.route('/strategy/record/<int:recordId>')
+def getStrategyInfo(recordId):
+	result={}
+	stocks=[]
+	oldStrategy = getStrategy(recordId)
+	result['recordName'] = oldStrategy.name
+	result['recordTime'] = oldStrategy.time
+	# result['todayBenefit']
+	tempRate = 0
+	for i in range(10):
+		stockId =oldStrategy.items[i].stock_id
+		temp = {}
+		temp['buyRate'] = oldStrategy.items[i].buy_rate
+		temp['stockId']=stockId
+		stock=getAStockBasicData(stockId)
+		temp['stockName']=stock.name
+		temp['currentPrice']=stock.trade
+		temp['trend']=stock.changepercent
+		temp['turnoverRate']=stock.turnoverratio
+		temp['marketProfitability']=stock.per
+		temp['todayVolume']=stock.volume
+		tempRate = tempRate + temp['buyRate']*getMonthReturnLatest(temp['stockId'])
+		stocks.append(temp)
+	result['todayBenefit']=tempRate
+	result['stocks']=stocks
+
+	return json.dumps(result,ensure_ascii=False)
+# 根据recordId获得需要回测的策略
+# recordId是long
+
+
+@app.route('/strategy/loopback/<int:recordId>')
+def getLoopbackInfo(recordId):
+	oldStrategy = getStrategy(recordId)
+	profitList = [0,0,0,0,0,0,0,0,0,0,0,0]
+	for i in range(10):
+		stockId =oldStrategy.items[i].stock_id
+		temp = {}
+		temp['buyRate'] = oldStrategy.item[i].buy_rate
+		tempProfit =getMonthReturnPerYear(stockId)
+		for i in range(12):
+			tempProfit[i]=tempProfit[i]*temp['buyRate']
+		profitList =list_add(profitList,tempProfit)
+
+	loopbackData = []
+	loopbackPeryear =getSZData('2018-06')
+	monthList =getOneYearMonthList('2018-06')
+	for i in range(12):
+		temp = {}
+		temp['data']= monthList[i]
+		temp['上证指数']= loopbackPeryear.get(temp['data'])
+		temp['自选股'] = profitList[i]
+		loopbackData.append(temp)
+
+	result = loopbackData
+
+	return json.dumps(result,ensure_ascii=False)
+
+
+@app.route('/strategy/record/<int:recordId>')
+def getStrategyInfo(recordId):
+	result={}
+	stocks=[]
+	oldStrategy = getStrategy(recordId)
+	result['recordName'] = oldStrategy.name
+	result['recordTime'] = oldStrategy.time
+	# result['todayBenefit']
+	tempRate = 0
+	for i in range(10):
+		stockId =oldStrategy.items[i].stock_id
+		temp = {}
+		temp['buyRate'] = oldStrategy.items[i].buy_rate
+		tempProfit =getMonthReturnPerYear(stockId)
+		temp['stockId']=stockId
+		stock=getAStockBasicData(stockId)
+		temp['stockName']=stock.name
+		temp['currentPrice']=stock.trade
+		temp['trend']=stock.changepercent
+		temp['turnoverRate']=stock.turnoverratio
+		temp['marketProfitability']=stock.per
+		temp['todayVolume']=stock.volume
+		tempRate = tempRate + temp['buyRate']*getMonthReturnLatest(temp['stockId'])
+		stocks.append(temp)
+	result['todayBenefit']=tempRate
+	result['stocks']=stocks
+
+	return json.dumps(result,ensure_ascii=False)
+
 
 
 def getAStockBasicData(code):
@@ -100,6 +304,70 @@ def getSZData(date):
 		res[date]=(close-open)/open
 	return res
 
+def getTopX():
+	object = pd.read_csv('gupiao_profit.csv');
+	object = object.head(10)
+	idlist = object.values.tolist()
+	finaldic = []
+	for i in range(10):
+		stockNum ='%06d' % idlist[i][0]
+		finaldic.append([stockNum,idlist[i][1]])
+
+	return finaldic
+
+def getRiskDic():
+	object = pd.read_csv('VaR/stockchoice.csv')
+	object = object.tail(1).values.tolist()[0]
+	finaldic = {}
+	finaldic['value']= object[0]
+	finaldic['var']= object[1]
+	for i in range(10):
+		stockNum = '%06d' %object[2*i+2]
+		finaldic[stockNum]= object[2*i+3]
+
+	return finaldic
+
+def getProfitDic():
+	object = pd.read_csv('VaR/stockchoice.csv')
+	object = object.head(1).values.tolist()[0]
+	finaldic = {}
+	finaldic['value']= object[0]
+	finaldic['var']= object[1]
+	for i in range(10):
+		stockNum = '%06d' %object[2*i+2]
+		finaldic[stockNum]= object[2*i+3]
+
+	return finaldic
+
+
+def getMonthReturnPerYear(code):
+	date_necessity = '2018-07'
+	Object = pd.read_csv('gupiao/objects/'+code+'_20060101-20180823.csv')
+	Object = Object.get(['tradeDate', 'dailyReturnReinv'])
+	Object['month'] = Object['tradeDate'].apply(lambda x: datetime.strftime(datetime.strptime(x, "%Y-%m-%d"), "%Y-%m"))
+
+	new_Object = pd.DataFrame(columns=['month', 'dailyReturnReinv'])
+	for (i, (month, dat)) in enumerate(Object.groupby('month')):
+		if month >= date_necessity:
+			break
+		new_Object.loc[i, ['month']] = month
+		new_Object.loc[i, ['dailyReturnReinv']] = dat[['dailyReturnReinv']].apply(np.mean)
+
+	return new_Object.tail(12).get('dailyReturnReinv').values.tolist()
+
+
+def getMonthReturnLatest(code):
+	Object = pd.read_csv('gupiao/objects/'+code+'_20060101-20180823.csv')
+	Object = Object.get(['tradeDate', 'dailyReturnReinv'])
+	Object['month'] = Object['tradeDate'].apply(lambda x: datetime.strftime(datetime.strptime(x, "%Y-%m-%d"), "%Y-%m"))
+
+	new_Object = pd.DataFrame(columns=['month', 'dailyReturnReinv'])
+	for (i, (month, dat)) in enumerate(Object.groupby('month')):
+		new_Object.loc[i, ['month']] = month
+		new_Object.loc[i, ['dailyReturnReinv']] = dat[['dailyReturnReinv']].apply(np.mean)
+
+	return new_Object.tail(1).get('dailyReturnReinv').values.tolist()[0]
+
 
 
 if __name__ == "__main__" :
@@ -108,6 +376,6 @@ if __name__ == "__main__" :
 	# print(getAStock('000001'))
 	# getStrategy(12232323)
 	# app.run(debug=True)
-	print(getSZData('2018-08'))
+	print(getMonthReturnLatest('000520'))
 
 
---
+++ test.py
@@ -0,0 +1,215 @@
+import math, os, re, time, random
+from datetime import datetime, timedelta, date
+import numpy as np
+import pandas as pd
+
+from sklearn.decomposition import PCA
+import statsmodels.api as sm
+from statsmodels import regression
+from sklearn import linear_model
+from sklearn.preprocessing import normalize
+
+import numpy as np, pandas as pd, math
+
+date_necessity = '2018-07'
+
+
+## 去重并得到相应数据的组合
+## input: path
+## output: (feature_file, object_file)
+def get_files(path='./gupiao/'):
+    ## get paths
+    def feature_file(number):
+        return path + 'features/' + number + '_20060101-20180725.csv'
+
+    ## get paths
+    def object_file(number):
+        return path + 'objects/' + number + '_20060101-20180823.csv'
+
+    ## test if last date is valid
+    def lastdate_is_valid(x):
+        pd_test1 = pd.read_csv(feature_file(x))
+        return ('tradeDate' in pd_test1.columns) \
+               and (len(pd_test1) > 1) \
+               and (date_necessity in str(pd_test1.tradeDate.iloc[-1]))
+    feature_numbers = {x.split('_')[0] for x in os.listdir(path + 'features')}
+
+    object_numbers = {x.split('_')[0] for x in os.listdir(path + 'objects')}
+
+    file_numbers = feature_numbers.intersection(object_numbers)
+    file_names = [(feature_file(x), object_file(x)) for x in file_numbers if lastdate_is_valid(x) == True]
+
+    print(len(file_names))
+    return file_names
+
+
+## input: (feature_file, object_file)
+## output: pd.Dataframe
+def get_data(file_pair):
+    (feature_file, object_file) = file_pair
+
+    feature = pd.read_csv(feature_file)
+
+    #### CURRENTLY USING ALL FEATURES
+    #     pca_factors = ['LCAP','LFLO','EquityFixedAssetRatio','DAVOL20','Volatility','DAREC','ROA','OperatingRevenueGrowRate']
+    pca_factors = ['LCAP', 'LFLO', 'PE', 'PB', 'PCF', 'PS', 'ROE', 'ROA', 'EPS', 'NetProfitRatio', 'GrossIncomeRatio',
+                   'OperatingRevenueGrowRate', 'OperatingProfitGrowRate', 'OperCashGrowRate', 'DebtsAssetRatio',
+                   'FixAssetRatio', 'CurrentRatio', 'QuickRatio', 'KDJ_K', 'BIAS20', 'BIAS60', 'PSY', 'VOL20', 'VOL60',
+                   'Volatility', 'AR', 'BR', 'REC', 'DAREC']
+    #### CURRENTLY USING ALL FEATURES
+
+    pca_factors = list(set(list(feature.columns)).intersection(pca_factors))
+    feature = feature.get(['tradeDate'] + pca_factors)
+
+    feature['month'] = feature['tradeDate'].apply(
+        lambda x: datetime.strftime(datetime.strptime(x, "%Y-%m-%d"), "%Y-%m"))
+
+    #### VERY IMPORTANT
+    #     feature = feature.fillna(feature.mean())
+    feature.iloc[-5:] = feature.iloc[-5:].fillna(method='pad')
+    feature.dropna(axis=1)
+    feature.reset_index(drop=1)
+    #### VERY IMPORTANT
+
+    Object = pd.read_csv(object_file)
+    Object = Object.get(['tradeDate', 'dailyReturnReinv'])
+    Object['month'] = Object['tradeDate'].apply(lambda x: datetime.strftime(datetime.strptime(x, "%Y-%m-%d"), "%Y-%m"))
+
+    new_feature = pd.DataFrame(columns=['month'] + pca_factors)
+    for (i, (month, dat)) in enumerate(feature.groupby('month')):
+        new_feature.loc[i, ['month']] = month
+        new_feature.loc[i, pca_factors] = dat[pca_factors].apply(np.mean)
+
+    new_Object = pd.DataFrame(columns=['month', 'dailyReturnReinv'])
+    for (i, (month, dat)) in enumerate(Object.groupby('month')):
+        new_Object.loc[i, ['month']] = month
+        new_Object.loc[i, ['dailyReturnReinv']] = dat[['dailyReturnReinv']].apply(np.mean)
+
+    new_data = pd.merge(new_feature, new_Object, how='left', on=['month'])
+    new_data.sort_values(by="month", ascending=True, inplace=True)
+
+    new_data.iloc[-5:] = new_data.iloc[-5:].fillna(method='pad')
+    new_data.dropna(inplace=True)
+    new_data.reset_index(drop=True, inplace=True)
+    new_data.loc[new_data.shape[0] - 1, new_data.columns[-1]] = math.nan
+
+    if date_necessity not in new_data.month.iloc[-1]:
+        raise ValueError('No data of current month {}'.format(date_necessity))
+
+    return new_data
+
+
+def get_data(file_pair):
+    (feature_file, object_file) = file_pair
+
+    feature = pd.read_csv(feature_file)
+
+    #### CURRENTLY USING ALL FEATURES
+    #     pca_factors = ['LCAP','LFLO','EquityFixedAssetRatio','DAVOL20','Volatility','DAREC','ROA','OperatingRevenueGrowRate']
+    pca_factors = ['LCAP', 'LFLO', 'PE', 'PB', 'PCF', 'PS', 'ROE', 'ROA', 'EPS', 'NetProfitRatio', 'GrossIncomeRatio',
+                   'OperatingRevenueGrowRate', 'OperatingProfitGrowRate', 'OperCashGrowRate', 'DebtsAssetRatio',
+                   'FixAssetRatio', 'CurrentRatio', 'QuickRatio', 'KDJ_K', 'BIAS20', 'BIAS60', 'PSY', 'VOL20', 'VOL60',
+                   'Volatility', 'AR', 'BR', 'REC', 'DAREC']
+    #### CURRENTLY USING ALL FEATURES
+
+    pca_factors = list(set(list(feature.columns)).intersection(pca_factors))
+    feature = feature.get(['tradeDate'] + pca_factors)
+
+    feature['month'] = feature['tradeDate'].apply(
+        lambda x: datetime.strftime(datetime.strptime(x, "%Y-%m-%d"), "%Y-%m"))
+
+    #### VERY IMPORTANT
+    #     feature = feature.fillna(feature.mean())
+    feature.iloc[-5:] = feature.iloc[-5:].fillna(method='pad')
+    feature.dropna(axis=1)
+    feature.reset_index(drop=1)
+    #### VERY IMPORTANT
+
+    Object = pd.read_csv(object_file)
+    Object = Object.get(['tradeDate', 'dailyReturnReinv'])
+    Object['month'] = Object['tradeDate'].apply(lambda x: datetime.strftime(datetime.strptime(x, "%Y-%m-%d"), "%Y-%m"))
+
+    new_feature = pd.DataFrame(columns=['month'] + pca_factors)
+    for (i, (month, dat)) in enumerate(feature.groupby('month')):
+        new_feature.loc[i, ['month']] = month
+        new_feature.loc[i, pca_factors] = dat[pca_factors].apply(np.mean)
+
+    new_Object = pd.DataFrame(columns=['month', 'dailyReturnReinv'])
+    for (i, (month, dat)) in enumerate(Object.groupby('month')):
+        new_Object.loc[i, ['month']] = month
+        new_Object.loc[i, ['dailyReturnReinv']] = dat[['dailyReturnReinv']].apply(np.mean)
+
+    new_data = pd.merge(new_feature, new_Object, how='left', on=['month'])
+    new_data.sort_values(by="month", ascending=True, inplace=True)
+
+    new_data.iloc[-5:] = new_data.iloc[-5:].fillna(method='pad')
+    new_data.dropna(inplace=True)
+    new_data.reset_index(drop=True, inplace=True)
+    new_data.loc[new_data.shape[0] - 1, new_data.columns[-1]] = math.nan
+
+    if date_necessity not in new_data.month.iloc[-1]:
+        raise ValueError('No data of current month {}'.format(date_necessity))
+
+    return new_data
+
+
+## input: pd.Dataframe
+## output: pd.Dataframe (with principle components)
+def get_pca_data(data, describe_ratio=0.99):
+    df = data[data.columns[1:-1]]
+    df = df.values
+    ## normalize principle components
+    #     df = normalize(df, norm='l2', axis=1, copy=True, return_norm=False)
+    df = scale(df)
+    pca = PCA(copy=True, n_components=describe_ratio, svd_solver='full')
+
+    pca.fit_transform(df)
+    low_d = pca.transform(df)
+
+    #     low_d = normalize(low_d, norm='l2', axis=1, copy=True, return_norm=False)
+    low_d = scale(low_d)
+
+    pr = pd.DataFrame(low_d, dtype='float64')
+    pr.columns = ['pr_{}'.format(i) for i in range(len(pr.columns))]
+    ## get data with only principle components
+    data = pd.concat([data[data.columns[0]], pr, data[data.columns[-1]]], axis=1)
+
+    return data
+
+
+def getMonthReturnPerYear(code):
+    Object = pd.read_csv('gupiao/objects/'+code+'_20060101-20180823.csv')
+    Object = Object.get(['tradeDate', 'dailyReturnReinv'])
+    Object['month'] = Object['tradeDate'].apply(lambda x: datetime.strftime(datetime.strptime(x, "%Y-%m-%d"), "%Y-%m"))
+
+    new_Object = pd.DataFrame(columns=['month', 'dailyReturnReinv'])
+    for (i, (month, dat)) in enumerate(Object.groupby('month')):
+        new_Object.loc[i, ['month']] = month
+        new_Object.loc[i, ['dailyReturnReinv']] = dat[['dailyReturnReinv']].apply(np.mean)
+
+    return new_Object.tail(12).get('dailyReturnReinv').values.tolist()
+
+if __name__ == '__main__':
+
+    print(getMonthReturnPerYear('600220'))
+
+    # selection = {}
+    # for file_name in get_files():
+    #     try:
+    #         data = get_data(file_name)
+    #         pca_data = get_pca_data(data)
+    #         date = pca_data.iloc[-1, 0]
+    #         dateyear = str(date).split('-')[0]
+    #         datemonth = str(date).split('-')[1]
+    #         score = get_regression_model(pca_data)
+    #         name = file_name[0].split('/')[-1].split('_')[0]
+    #
+    #         selection[name] = score
+    #
+    #     except Exception as err:
+    #         pass
+    #
+    # sorted_selection = sorted(selection.items(), key=lambda x: x[1], reverse=True)
+    # output_df = pd.DataFrame({'id': [str(x[0]) for x in sorted_selection],'profit': [float(x[1]) for x in sorted_selection]})
+    #
+    # output_df.to_csv("gupiao_profit.csv", index=False, sep=',')
